1b3fc4025e0886a55ea6021a4f5f463c7b256603
kewlyog09 <kewlyog09@gmail.com>
2023-03-24 02:23:43 +0530

    code files added

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 // this is first line of code
 // second line
 // third line -- for new branch
+
+provider "aws" {
+    region = "us-east-2"
+    shared_credentials_files = ["~/.aws/credentials"]
+}
